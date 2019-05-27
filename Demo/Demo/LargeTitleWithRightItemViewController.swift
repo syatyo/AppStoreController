47adde0cfae459e2bb4516b3ff02f4ef22d2c047
syatyo <yoshiharuseijin@gmail.com>
2019-05-28 00:49:09 +0900

    feat: 🎸 evil implementation using UIKit private class

--- Demo/Demo/LargeTitleWithRightItemViewController.swift
+++ Demo/Demo/LargeTitleWithRightItemViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class LargeTitleWithRightItemViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
